6bb0db9c3c9757041b4c59ef12eb1986ebedd28f
SoulAttacker <soulwalkerilyf@gmail.com>
2025-12-10 17:35:31 +1100

    replace smartparens with fingertip

---
+++ elisp/init-fingertip.el
@@ -0,0 +1,117 @@
+;;; init-fingertip.el --- fingertip configuration    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  soulwalker
+
+;; Author: soulwalker <soulwalker@Mac>
+;; Keywords: fingertip
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'fingertip)
+
+(dolist (hook (list
+               'c-mode-common-hook
+               'c-mode-hook
+               'c++-mode-hook
+               'java-mode-hook
+               'haskell-mode-hook
+               'emacs-lisp-mode-hook
+               'lisp-interaction-mode-hook
+               'lisp-mode-hook
+               'maxima-mode-hook
+               'ielm-mode-hook
+               'sh-mode-hook
+               'makefile-gmake-mode-hook
+               'php-mode-hook
+               'python-mode-hook
+               'js-mode-hook
+               'go-mode-hook
+               'qml-mode-hook
+               'jade-mode-hook
+               'css-mode-hook
+               'ruby-mode-hook
+               'coffee-mode-hook
+               'rust-mode-hook
+               'rust-ts-mode-hook
+               'qmake-mode-hook
+               'lua-mode-hook
+               'swift-mode-hook
+               'web-mode-hook
+               'markdown-mode-hook
+               'llvm-mode-hook
+               'conf-toml-mode-hook
+               'nim-mode-hook
+               'typescript-mode-hook
+               'c-ts-mode-hook
+               'c++-ts-mode-hook
+               'cmake-ts-mode-hook
+               'toml-ts-mode-hook
+               'css-ts-mode-hook
+               'js-ts-mode-hook
+               'json-ts-mode-hook
+               'python-ts-mode-hook
+               'bash-ts-mode-hook
+               'typescript-ts-mode-hook
+               'zig-mode-hook
+               'zig-ts-mode-hook
+               ))
+  (add-hook hook #'(lambda () (fingertip-mode 1))))
+
+(define-key fingertip-mode-map (kbd "(") 'fingertip-open-round)
+(define-key fingertip-mode-map (kbd "[") 'fingertip-open-bracket)
+(define-key fingertip-mode-map (kbd "{") 'fingertip-open-curly)
+(define-key fingertip-mode-map (kbd ")") 'fingertip-close-round)
+(define-key fingertip-mode-map (kbd "]") 'fingertip-close-bracket)
+(define-key fingertip-mode-map (kbd "}") 'fingertip-close-curly)
+(define-key fingertip-mode-map (kbd "=") 'fingertip-equal)
+
+(define-key fingertip-mode-map (kbd "（") 'fingertip-open-chinese-round)
+(define-key fingertip-mode-map (kbd "「") 'fingertip-open-chinese-bracket)
+(define-key fingertip-mode-map (kbd "【") 'fingertip-open-chinese-curly)
+(define-key fingertip-mode-map (kbd "）") 'fingertip-close-chinese-round)
+(define-key fingertip-mode-map (kbd "」") 'fingertip-close-chinese-bracket)
+(define-key fingertip-mode-map (kbd "】") 'fingertip-close-chinese-curly)
+
+(define-key fingertip-mode-map (kbd "%") 'fingertip-match-paren)
+(define-key fingertip-mode-map (kbd "\"") 'fingertip-double-quote)
+(define-key fingertip-mode-map (kbd "'") 'fingertip-single-quote)
+
+(define-key fingertip-mode-map (kbd "SPC") 'fingertip-space)
+(define-key fingertip-mode-map (kbd "RET") 'fingertip-newline)
+
+(define-key fingertip-mode-map (kbd "M-o") 'fingertip-backward-delete)
+(define-key fingertip-mode-map (kbd "C-d") 'fingertip-forward-delete)
+(define-key fingertip-mode-map (kbd "C-k") 'fingertip-kill)
+
+(define-key fingertip-mode-map (kbd "M-\"") 'fingertip-wrap-double-quote)
+(define-key fingertip-mode-map (kbd "M-'") 'fingertip-wrap-single-quote)
+(define-key fingertip-mode-map (kbd "M-[") 'fingertip-wrap-bracket)
+(define-key fingertip-mode-map (kbd "M-{") 'fingertip-wrap-curly)
+(define-key fingertip-mode-map (kbd "M-(") 'fingertip-wrap-round)
+(define-key fingertip-mode-map (kbd "M-)") 'fingertip-unwrap)
+
+(define-key fingertip-mode-map (kbd "M-p") 'fingertip-jump-right)
+(define-key fingertip-mode-map (kbd "M-n") 'fingertip-jump-left)
+(define-key fingertip-mode-map (kbd "M-:") 'fingertip-jump-out-pair-and-newline)
+
+(define-key fingertip-mode-map (kbd "C-j") 'fingertip-jump-up)
+
+(provide 'init-fingertip)
+;;; init-fingertip.el ends here
